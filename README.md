7a0bef9e5b946f43cd33c5e84c6a4a71ec733e5a
Asad Faraz <asadfaraz44@gmail.com>
2021-04-03 11:52:17 +0500

    Final Counter

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Simple Counter App React
+
+## Details
+
+This is a simple counter made in React.js. You can increase the counter by one, decrease
+the counter by 1 or reset the counter to 0.
+
+Also I added some CSS to make things little nicer and beautiful. I added some inline Style
+in JSX conditionally. When the value of counter is greater that zero then the color
+of the counter text will be green and when the text is less then zero the color will be
+red.
+
+I split the button events so I can learn how it all works. For example I used inline
+functions in onclick of button and I also used an external function.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  // UseState for counter text
+  const [counterText, setCounterText] = useState(0);
+  // Function to increment the counter
+  const increaseCounter = () => {
+    setCounterText(counterText + 1);
+    if (counterText > 10) {
+    }
+  };
+
+  let colr = "blue";
+  if (counterText > 0) {
+    colr = "green";
+  }
+  if (counterText < 0) {
+    colr = "red";
+  }
+
+  return (
+    <section className="App">
+      <h1>Very Simple Counter in React</h1>
+
+      <h2 style={{ color: colr }}>{counterText}</h2>
+
+      <button
+        type="button"
+        onClick={() => {
+          setCounterText(counterText - 1);
+        }}
+      >
+        Decrease
+      </button>
+      <button type="button" onClick={() => setCounterText(0)}>
+        Reset
+      </button>
+      <button type="button" onClick={increaseCounter}>
+        Increase
+      </button>
+    </section>
+  );
 }
 
 export default App;
